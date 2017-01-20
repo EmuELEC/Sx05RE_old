34113159989ba0a8085242e58791c6e3cda4027a
Alexandr <alex@alexelec.in.ua>
2017-01-20 13:06:11 +0200

    upd Alexelec-settings (add Tvheadend)

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="732def4"
+PKG_VERSION="a911d57"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
