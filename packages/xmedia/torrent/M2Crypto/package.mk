ece99348de8fbbd2d9655a47728785d716474583
Alexandr <alex@alexelec.in.ua>
2017-01-23 10:22:29 +0200

    upd M2Crypto-0.25.1

--- packages/xmedia/torrent/M2Crypto/package.mk
+++ packages/xmedia/torrent/M2Crypto/package.mk
@@ -4,12 +4,12 @@
 ################################################################################
 
 PKG_NAME="M2Crypto"
-PKG_VERSION="0.22.6rc4"
+PKG_VERSION="0.25.1"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="https://pypi.python.org/pypi/M2Crypto"
-PKG_URL="http://pypi.python.org/packages/source/M/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://pypi.python.org/packages/9c/58/7e8d8c04995a422c3744929721941c400af0a2a8b8633f129d92f313cfb8/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain Python distutilscross:host"
 PKG_SECTION="xmedia/torrent"
 PKG_SHORTDESC="M2Crypto is the most complete Python wrapper for OpenSSL"
@@ -23,7 +23,7 @@ pre_configure_target() {
 }
 
 make_target() {
-  python setup.py build --cross-compile build_ext --openssl=$LIB_PREFIX
+  python setup.py build build_ext --openssl=$LIB_PREFIX
 }
 
 makeinstall_target() {
